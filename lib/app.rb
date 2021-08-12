4bea2fbbf6bfe173691ce765e367fa5c2fa53432
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-08-12 13:14:37 +0100

    Enabled sessions, redirected POST

--- lib/app.rb
+++ lib/app.rb
@@ -6,13 +6,21 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get "/" do
     erb(:index)
   end
 
   post "/names" do
-    @name1 = params[:name1]
-    @name2 = params[:name2]
+    session[:name1] = params[:name1]
+    session[:name2] = params[:name2]
+    redirect "/play"
+  end
+
+  get "/play" do
+    @name1 = session[:name1]
+    @name2 = session[:name2]
     erb(:play)
   end
 
