0f11c78d49118a0da5ed86f7edc849d3329cf426
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-08-12 14:49:18 +0100

    Created unit test for attack, passed

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,15 @@
 class Player
+  attr_reader :name, :hp
 
-  attr_reader :name
+  DEFAULT_HP = 60
+  DEFAULT_ATTACK = 10
 
   def initialize(name)
     @name = name
+    @hp = DEFAULT_HP
   end
 
+  def is_attacked
+    @hp -= DEFAULT_ATTACK
+  end
 end
--- spec/unit/player_spec.rb
+++ spec/unit/player_spec.rb
@@ -1,8 +1,21 @@
 require 'player'
 
 describe Player do
-  it "can return a player's name" do
-  test = Player.new("TestName")
-  expect(test.name).to eq("TestName")
+  let(:subject) { Player.new("Joe") }
+
+  describe "#initialize" do
+    it "can return a player's name" do
+      expect(subject.name).to eq("Joe")
+    end
+
+    it "initialize with full HP" do
+      expect(subject.hp).to eq(Player::DEFAULT_HP)
+    end
+  end
+
+  describe "#is_attacked" do
+    it "reduce player's HP by default attack" do
+       expect { subject.is_attacked }.to change { subject.hp }.by(-Player::DEFAULT_ATTACK)
+    end
   end
 end
